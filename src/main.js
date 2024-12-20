1ff0bd5b8602fda1525d8a5dd76a1835b11f5b9f
tsugaide <arjunazarqa62@gmail.com>
2024-12-20 19:39:36 +0700

    update5

--- src/main.js
+++ src/main.js
@@ -2,9 +2,4 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/output.css";
 
-if (process.env.NODE_ENV === "production") {
-  Vue.config.devtools = false;
-  Vue.config.productionTip = false;
-}
-
 createApp(App).mount("#app");
